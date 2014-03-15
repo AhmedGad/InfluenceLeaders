9c8efd65236c08544279db94db9395af342e354d
AhmedGad <ahmed.mu.gad@gmail.com>
2014-03-15 22:03:29 +0200

    Editing GraphIndexing class

--- src/graph/GraphIndexing.java
+++ src/graph/GraphIndexing.java
@@ -6,45 +6,41 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 /**
- * Given the Graph files this class will extract every user as a file that contains all followers for this user<br>
+ * Given the Graph files this class will extract every user as a file that
+ * contains all followers for this user<br>
  * the input for this class is : <br>
  * <ul>
- * <li> graphDir : the directory of graph files.</li>
- * <li> indexedDir : the directory of the output files, the files that will contain every user's follower list.</li>
- * <li> setDir : directory of the HashSet that contains graph files names, because in case the class stop don't begin the
- * whole process again just begin from the last file, it can be any directory <br> 
+ * <li>graphDir : the directory of graph files.</li>
+ * <li>indexedDir : the directory of the output files, the files that will
+ * contain every user's follower list.</li>
+ * <li>setDir : directory of the HashSet that contains graph files names,
+ * because in case the class stop don't begin the whole process again just begin
+ * from the last file, it can be any directory <br>
  * the HashSet file will be created after the first run</li>
  * </ul>
  */
 public class GraphIndexing {
 
-	static byte[] bytes = new byte[600000000];
+	static byte[] bytes = new byte[800000000];
 
 	public final static int READ_FOLLOWERS = 0;
 	public final static int NEW_USER = 1;
 	public final static int FIRST_LINE_AFTER_NEW_USER = 2;
-	public final static String indexedDir = "/media/hanafy/New Volume/Lectures/4th year/GP/Data Collected/Indexed/";
-	public final static String setDir = "/media/hanafy/New Volume/Lectures/4th year/GP/Data Collected/";
-	public final static String graphDir = "/media/hanafy/New Volume/Lectures/4th year/GP/Data Collected/Graph";
+	public final static String indexedDir = "./Users/";
+	public final static String graphDir = "./Graph/";
 
-	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 		File inputFile = new File(graphDir);
+		BufferedWriter errorLog = new BufferedWriter(new FileWriter(new File(
+				"errorlog.txt")));
 
-		HashSet<String> fileSet = readHashSet(setDir);
 		for (File f : inputFile.listFiles()) {
-			if (f.getName().startsWith("graph") && f.getName().endsWith(".txt") && !fileSet.contains(f.getName())) {
+			if (f.getName().startsWith("graph") && f.getName().endsWith(".txt")) {
 				System.out.println(f.getAbsolutePath());
 
 				int state = NEW_USER;
@@ -52,14 +48,21 @@ public class GraphIndexing {
 
 				long t1 = System.currentTimeMillis();
 				FileInputStream fis;
-				DataInputStream dis = new DataInputStream(fis = new FileInputStream(f));
+				DataInputStream dis = new DataInputStream(
+						fis = new FileInputStream(f));
 				int size = dis.available(), loaded = dis.read(bytes);
-				if (size != loaded) {
-					throw new Exception(f.getAbsolutePath() + " loaded: " + loaded + " total Size: " + size);
+				if (size != loaded && loaded > 0) {
+					System.err.println(f.getAbsolutePath() + " loaded: "
+							+ loaded + " total Size: " + size);
+					errorLog.write(f.getAbsolutePath() + " loaded: " + loaded
+							+ " total Size: " + size + "\n");
+					errorLog.flush();
 				}
 
-				ByteArrayInputStream in = new ByteArrayInputStream(bytes, 0, size);
-				BufferedReader buff = new BufferedReader(new InputStreamReader(in));
+				ByteArrayInputStream in = new ByteArrayInputStream(bytes, 0,
+						size);
+				BufferedReader buff = new BufferedReader(new InputStreamReader(
+						in));
 
 				BufferedWriter writer = null;
 
@@ -80,55 +83,38 @@ public class GraphIndexing {
 					} else if (state == NEW_USER) {
 						NumUsers++;
 						StringTokenizer tok = new StringTokenizer(s);
-						writer = new BufferedWriter(new FileWriter(new File(indexedDir + tok.nextToken())));
+						writer = new BufferedWriter(new FileWriter(new File(
+								indexedDir + tok.nextToken())));
 						if (!tok.nextToken().equals("followers:")) {
-							throw new Exception("\"followers:\" word not found!!" + "\tcur line: " + s);
+							System.err.println(f.getAbsolutePath()
+									+ "\t\"followers:\" word not found!!"
+									+ "\tcur line: " + s);
+							errorLog.write(f.getAbsolutePath() + "\t"
+									+ "\"followers:\" word not found!!"
+									+ "\tcur line: " + s + "\n");
 						}
 						state++;
 					} else if (state == FIRST_LINE_AFTER_NEW_USER) {
 						if (s.length() > 0)
-							throw new Exception("first line is not empty!!!!" + "\tcur line: " + s);
+							System.err
+									.println(f.getAbsolutePath()
+											+ "\tfirst line is not empty!!!!\tcur line: "
+											+ s);
+						errorLog.write(f.getAbsolutePath()
+								+ "\tfirst line is not empty!!!!\tcur line: "
+								+ s + "\n");
 						state = 0;
 					}
 				}
 
 				fis.close();
 				dis.close();
-				System.out.println(NumUsers + " " + (System.currentTimeMillis() - t1));
-
-				// write HashSet
-				fileSet.add(f.getName());
-				writeHashSet(fileSet, setDir);
+				System.out.println(NumUsers + " "
+						+ (System.currentTimeMillis() - t1));
 			}
 		}
 
+		errorLog.close();
 		System.out.println("Finished");
-	}
-
-	private static void writeHashSet(HashSet<String> set, String directory) throws IOException {
-		String dir = directory + "graphSet";
-		FileOutputStream fout = new FileOutputStream(dir);
-		ObjectOutputStream oos = new ObjectOutputStream(fout);
-		oos.writeObject(set);
-
-		oos.close();
-		fout.close();
-	}
-
-	private static HashSet<String> readHashSet(String directory) throws IOException, ClassNotFoundException {
-		String dir = directory + "graphSet";
-		FileInputStream fin;
-		try {
-			fin = new FileInputStream(dir);
-		} catch (FileNotFoundException e) {
-			writeHashSet(new HashSet<String>(), directory);
-			fin = new FileInputStream(dir);
-		}
-
-		ObjectInputStream ois = new ObjectInputStream(fin);
-		HashSet<String> set = (HashSet<String>) ois.readObject();
-		ois.close();
-
-		return set;
 	}
 }
