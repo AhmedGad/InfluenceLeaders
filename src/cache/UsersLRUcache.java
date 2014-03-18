4b6ac8542167ba7705920ddd7c27bf668ecfc324
AhmedGad <ahmed.mu.gad@gmail.com>
2014-03-18 16:51:28 +0200

    bug fixed

--- src/cache/UsersLRUcache.java
+++ src/cache/UsersLRUcache.java
@@ -1,13 +1,15 @@
 package cache;
 
 import graph.UserFollowers;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class UsersLRUcache {
 	private int capacity;
-	ConcurrentHashMap<Long, UserFollowers> hm;
-	ConcurrentLinkedDeque<Long> queue;
+	ConcurrentHashMap<Long, UserFollowers> hashMap;
+	Deque<Long> dequeue;
 
 	/**
 	 * 
@@ -16,19 +18,22 @@ public class UsersLRUcache {
 	 */
 	public UsersLRUcache(int capacity) {
 		this.capacity = capacity;
-		queue = new ConcurrentLinkedDeque<Long>();
-		hm = new ConcurrentHashMap<Long, UserFollowers>();
+		dequeue = new ArrayDeque<Long>();
+		hashMap = new ConcurrentHashMap<Long, UserFollowers>();
 	}
 
-	public void add(Long key, UserFollowers value) {
-		hm.put(key, value);
-		capacity -= value.size();
-		while (capacity < 0) {
-			capacity += hm.remove(queue.removeLast()).size();
+	public synchronized void add(Long key, UserFollowers value) {
+		if (!hashMap.contains(key)) {
+			hashMap.put(key, value);
+			dequeue.addFirst(key);
+			capacity -= value.size();
+			while (capacity < 0) {
+				capacity += hashMap.remove(dequeue.removeLast()).size();
+			}
 		}
 	}
 
 	public UserFollowers get(Long key) {
-		return hm.get(key);
+		return hashMap.get(key);
 	}
 }
--- src/graph/FollowingGraph.java
+++ src/graph/FollowingGraph.java
@@ -3,7 +3,7 @@ package graph;
 import cache.UsersLRUcache;
 
 /**
- *	the Graph Class that implements Graph interface 
+ * the Graph Class that implements Graph interface
  */
 
 public class FollowingGraph implements Graph {
@@ -23,15 +23,18 @@ public class FollowingGraph implements Graph {
 	/**
 	 * @return true if u1 is following u2
 	 * @throws Exception
-	 *            if u2 doesn't exist
+	 *             if u2 doesn't exist
 	 */
 	@Override
 	public boolean isFollowing(long u1, long u2) throws Exception {
 		UserFollowers u2followers = cache.get(u2);
 		if (u2followers == null) {
-			u2followers = new UserFollowers(u2, FollowersReader.loadFollowers(
-					u2, dir));
-			cache.add(u2, u2followers);
+			u2followers = cache.get(u2);
+			if (u2followers == null) {
+				u2followers = new UserFollowers(u2,
+						FollowersReader.loadFollowers(u2, dir));
+				cache.add(u2, u2followers);
+			}
 		}
 		return u2followers.hasFollower(u1);
 	}
